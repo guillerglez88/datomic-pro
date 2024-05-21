76e0d24001dfb92d2a1b157ef90fd806b0b2fde6
Guillermo <guillerglez88@gmail.com>
2024-05-21 21:56:17 +0200

    ability to set datomic host and alt-host properties

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ echo "sql-user=$POSTGRES_USER" >> ./config/transactor.properties
 echo "sql-password=$POSTGRES_PASSWORD" >> ./config/transactor.properties
 echo "storage-admin-password=$DATOMIC_STORAGE_ADMIN_PASSWORD" >> ./config/transactor.properties
 echo "storage-datomic-password=$DATOMIC_STORAGE_DATOMIC_PASSWORD" >> ./config/transactor.properties
+echo "host=$DATOMIC_HOST" >> ./config/transactor.properties
+echo "alt-host=$DATOMIC_ALT_HOST" >> ./config/transactor.properties
 
 # start postgres entrypoint
 docker-entrypoint.sh -c 'shared_buffers=2GB' &
